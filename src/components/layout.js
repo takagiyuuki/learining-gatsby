170184de02a503ef4a4172984a6db8715e6a6650
Yuuki <snow7777man@gmail.com>
2023-05-16 08:45:46 +0000

    edit Pages

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+// src/components/layout.js
+import React from "react"
+import "../styles/layout.css"
+import Header from "./header"
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Header />
+            <main>{children}</main>
+        </div>
+    )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
-import * as React from "react"
-import "../styles/style.css"
+// /src/pages/index.js
+import React from "react"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <div>Hello world!</div>
+    </Layout>
+  )
 }
