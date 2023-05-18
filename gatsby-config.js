9483c37dc9a4da56814e4c7b30024eb9dba373c3
Yuuki <snow7777man@gmail.com>
2023-05-18 07:23:27 +0000

    edit gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,21 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
 module.exports = {
-  plugins: [],
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsbyで作成したブログサイトです。",
+    author: "Engineer X"
+  },
+  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY
+      }
+    }
+  ],
 }
